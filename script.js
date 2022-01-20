4074db2a500442e4a15aedce7a6876366b993866
Ahmad Fuad Amsyari <48084093+fuadamsyari@users.noreply.github.com>
2022-01-20 15:09:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
+container.addEventListener('click', function (e) {
+  if (e.target.className == 'thumb') {
+    jumbo.src = e.target.src;
+    jumbo.classList.add('fade');
+    setTimeout(() => {
+      jumbo.classList.remove('fade');
+    }, 500);
+    thumbs.forEach((thumb) => {
+      //   if (thumb.classList.contains('active')) {
+      //     // thumb.classList.remove('active');
+      //   }
+      thumb.className = 'thumb';
+    });
+    e.target.classList.add('active');
+  }
+});
